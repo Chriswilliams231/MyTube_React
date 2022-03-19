1e6a3582af8902038b4ad5ddb5904a0e146d0c5f
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2022-03-19 15:32:05 -0700

    first commit

---
+++ src/compontents/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import SearchBar from './SearchBar'
+import youtube from '../api/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+    }
+
+
+    onSeachSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+
+        this.setState({ videos: response.data.items })
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video })
+    };
+
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onSearchSubmit={this.onSeachSubmit} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            </div>
+        )
+    }
+}
+
+export default App;
